c3a521280c986e09904199bfffab6e30db9c3e48
Caique Ferreira <caique.garciatst@gmail.com>
2023-01-25 14:42:02 -0300

    Changes to be committed: 	modified:   forms/src/components/Forms.js 	modified:   fundamentos

--- forms/src/components/Forms.js
+++ forms/src/components/Forms.js
@@ -2,7 +2,17 @@ import './Forms.css'
 
 const Forms = () => {
   return (
-    <div>Forms</div>
+    <div>
+        <form>
+            <div>
+                <label htmlFor="nome">
+                    Nome:
+                </label>
+                <input type="text" name="name" placeholder="Digite seu nome." />
+            </div>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
   )
 }
 
